1e6610af02db1470f0d7f1656ec01a63829bf6e8
Rldumdum <juelierldumdum@gmail.com>
2024-02-07 21:28:39 +0800

    8-select-finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import "./App.css";
+import MuiSelect from "./components/MuiSelect";
 // import MuiTypography from "./components/MuiTypography";
 // import MuiButton from "./components/MuiButton";
-import MuiTextField from "./components/MuiTextField";
+// import MuiTextField from "./components/MuiTextField";
 
 function App() {
   return (
     <div className="App">
       {/* <MuiTypography /> */}
       {/* <MuiButton /> */}
-      <MuiTextField/>
+      {/* <MuiTextField/> */}
+      <MuiSelect />
     </div>
   );
 }
