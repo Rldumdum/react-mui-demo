6e650a41e997b4aaf8581bf9a45eb0f451cafbdd
Rldumdum <juelierldumdum@gmail.com>
2024-02-07 18:53:22 +0800

    initial commit

---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,30 @@
+import { Typography } from "@mui/material";
+
+const MuiTypography = () => {
+  return (
+    <div>
+      <Typography variant="h1">h1 Heading</Typography>
+      <Typography variant="h2">h2 Heading</Typography>
+      <Typography variant="h3">h3 Heading</Typography>
+      <Typography variant="h4">h4 Heading</Typography>
+      <Typography variant="h5">h5 Heading</Typography>
+      <Typography variant="h6">h6 Heading</Typography>
+
+      <Typography variant="subtitle1">Sub title 1</Typography>
+      <Typography variant="subtitle2">Sub title 2</Typography>
+
+      <Typography variant="body1">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, odio
+        quis officia laborum dignissimos nemo qui vitae iure. Nemo unde deserunt
+        neque modi mollitia cum aut, omnis voluptas consectetur. Fugit.
+      </Typography>
+      <Typography variant="body2">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, odio
+        quis officia laborum dignissimos nemo qui vitae iure. Nemo unde deserunt
+        neque modi mollitia cum aut, omnis voluptas consectetur. Fugit.
+      </Typography>
+    </div>
+  );
+};
+
+export default MuiTypography;
