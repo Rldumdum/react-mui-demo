ed912874335bd97cc4cdd9d33a61def7e7114aeb
Rldumdum <juelierldumdum@gmail.com>
2024-02-09 03:45:59 +0800

    34-alert-finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
-import MuiTable from "./components/MuiTable";
+import MuiAlert from "./components/MuiAlert";
+// import MuiTable from "./components/MuiTable";
 // import MuiTooltip from "./components/MuiTooltip";
 // import MuiBadge from "./components/MuiBadge";
 // import MuiChip from "./components/MuiChip";
@@ -52,7 +53,8 @@ function App() {
       {/* <MuiList/> */}
       {/* <MuiChip /> */}
       {/* <MuiTooltip /> */}
-      <MuiTable />
+      {/* <MuiTable /> */}
+      <MuiAlert/>
     </div>
   );
 }
