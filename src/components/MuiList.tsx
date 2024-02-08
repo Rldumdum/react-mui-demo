14a7065f7b4390e2249d6b101f76263c549411b2
Rldumdum <juelierldumdum@gmail.com>
2024-02-09 01:44:40 +0800

    30-list-finished

--- src/components/MuiList.tsx
+++ src/components/MuiList.tsx
@@ -1,17 +1,57 @@
-import { Box, List, ListItem, ListItemText } from "@mui/material";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemIcon,
+  ListItemAvatar,
+  Avatar,
+  ListItemButton,
+  Divider,
+} from "@mui/material";
+import MailIcon from "@mui/icons-material/Mail";
 
 const MuiList = () => {
   return (
     <Box sx={{ width: "400px", bgcolor: "#efefef" }}>
       <List>
         <ListItem>
-          <ListItemText primary="List item 1" />
+          <ListItemButton>
+            <ListItemIcon>
+              <ListItemAvatar>
+                <Avatar>
+                  <MailIcon />
+                </Avatar>
+              </ListItemAvatar>
+            </ListItemIcon>
+            <ListItemText primary="List item 1" secondary="Secondary text" />
+          </ListItemButton>
         </ListItem>
+        <Divider />
         <ListItem>
-          <ListItemText primary="List item 2" />
+          <ListItemButton>
+            <ListItemIcon>
+              <ListItemAvatar>
+                <Avatar>
+                  <MailIcon />
+                </Avatar>
+              </ListItemAvatar>
+            </ListItemIcon>
+            <ListItemText primary="List item 2" secondary="Secondary Text" />
+          </ListItemButton>
         </ListItem>
+        <Divider />
         <ListItem>
-          <ListItemText primary="List item 3" />
+          <ListItemButton>
+            <ListItemIcon>
+              <ListItemAvatar>
+                <Avatar>
+                  <MailIcon />
+                </Avatar>
+              </ListItemAvatar>
+            </ListItemIcon>
+            <ListItemText primary="List item 3" secondary="Secondary text" />
+          </ListItemButton>
         </ListItem>
       </List>
     </Box>
